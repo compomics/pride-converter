257815761cb9c2aa8d680f0818542c29241bdb53
florian.ebi <florian.ebi@gmail.com>
2010-04-16 13:18:23 +0000

    removed Java 6 annotation (for compatibility with Java 5).

--- pride_converter/prideconverter-core/src/main/java/no/uib/prideconverter/util/RadioButtonRenderer.java
+++ pride_converter/prideconverter-core/src/main/java/no/uib/prideconverter/util/RadioButtonRenderer.java
@@ -13,7 +13,6 @@ import javax.swing.table.*;
  */
 public class RadioButtonRenderer implements TableCellRenderer {
 
-    @Override
     public Component getTableCellRendererComponent(JTable table, Object value,
             boolean isSelected, boolean hasFocus, int row, int column) {
         if (value == null) {
